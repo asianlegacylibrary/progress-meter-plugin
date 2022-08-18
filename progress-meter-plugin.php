85b3d2acc54abcef547274e42f62bc821f49636f
Shkodenko V. Taras <taras.shkodenko@blackbit.de>
2022-08-19 00:46:40 +0300

    Fixed code style accoring to a WordPress CMS code style

--- progress-meter-plugin.php
+++ progress-meter-plugin.php
@@ -12,13 +12,13 @@
  * Domain Path:  /languages/
  *
  * @package     ProgressMeter
- * @author      Asian Legacy Library
+ * @author      Asian Legacy Library <taras@shkodenko.com>
  * @copyright   Copyright (c) 2022, Asian Legacy Library
  */
 
 // Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) {
-    exit;
+	exit;
 }
 
 require __DIR__ . '/vendor/autoload.php';
@@ -29,180 +29,170 @@ require __DIR__ . '/vendor/autoload.php';
  *
  * @since 1.0.0
  */
-class Progress_Meter
-{
-    /**
-     * @var string Plugin Options
-     */
-    const PROGRESS_METER_OPTIONS = 'progress_meter_options';
+class Progress_Meter {
 
-    /**
-     * @var string Plugin Name
-     */
-    public static $NAME = 'Progress Meter';
+	/**
+	 * @var string Plugin Options
+	 */
+	const PROGRESS_METER_OPTIONS = 'progress_meter_options';
 
-    /**
-     * @var string Plugin Version
-     */
-    public static $VER = '1.0.0';
+	/**
+	 * @var string Plugin Name
+	 */
+	public static $NAME = 'Progress Meter';
 
-    /**
-     * @var Progress_Meter The one true Progress_Meter
-     */
-    private static $instance;
+	/**
+	 * @var string Plugin Version
+	 */
+	public static $VER = '1.0.0';
 
-    private function __construct()
-    {
-        error_log(__METHOD__ . ' +' . __LINE__ . PHP_EOL);
-    }
+	/**
+	 * @var Progress_Meter The one true Progress_Meter
+	 */
+	private static $instance;
 
-    /**
-     * Main instance
-     *
-     * @return Progress_Meter
-     */
-    public static function instance()
-    {
-        error_log(__METHOD__ . ' +' . __LINE__ . PHP_EOL);
+	private function __construct() {
+		error_log( __METHOD__ . ' +' . __LINE__ . PHP_EOL );
+	}
 
-        if ( ! isset( self::$instance ) && ! ( self::$instance instanceof Progress_Meter ) ) {
-            self::$instance = new Progress_Meter;
-            self::$instance->init();
-        }
+	/**
+	 * Main instance
+	 *
+	 * @return Progress_Meter
+	 */
+	public static function instance() {
+		error_log( __METHOD__ . ' +' . __LINE__ . PHP_EOL );
 
-        return self::$instance;
-    }
+		if ( ! isset( self::$instance ) && ! ( self::$instance instanceof Progress_Meter ) ) {
+			self::$instance = new Progress_Meter();
+			self::$instance->init();
+		}
 
-    public function init()
-    {
-        error_log(__METHOD__ . ' +' . __LINE__ . PHP_EOL);
+		return self::$instance;
+	}
 
-        // This adds support for a "progress-meter" shortcode
-        add_shortcode('progress-meter', [$this, 'progressMeterWidget']);
+	public function init() {
+		error_log( __METHOD__ . ' +' . __LINE__ . PHP_EOL );
 
-        // @see: https://wpmudev.com/blog/schedule-functions-in-your-plugins-with-wordpress-cron/
-        add_filter('cron_schedules', [$this, 'addCsCron']);
-        add_action('updateSchedulerNotify', [$this, 'updateSchedulerNotify']);
-    }
+		// This adds support for a "progress-meter" shortcode
+		add_shortcode( 'progress-meter', array( $this, 'progressMeterWidget' ) );
 
-    public function updateSchedulerNotify()
-    {
-        error_log(__METHOD__ . ' +' . __LINE__ . PHP_EOL);
+		// @see: https://wpmudev.com/blog/schedule-functions-in-your-plugins-with-wordpress-cron/
+		add_filter( 'cron_schedules', array( $this, 'addCsCron' ) );
+		add_action( 'updateSchedulerNotify', array( $this, 'updateSchedulerNotify' ) );
+	}
 
-        $this->removePluginData();
+	public function updateSchedulerNotify() {
+		error_log( __METHOD__ . ' +' . __LINE__ . PHP_EOL );
 
-        $this->updatePluginData();
-    }
+		$this->removePluginData();
 
-    public function addCsCron()
-    {
-        error_log(__METHOD__ . ' +' . __LINE__ . PHP_EOL);
+		$this->updatePluginData();
+	}
 
-        // set our 24 hours, units in seconds
-        $period = 24 * 3600;
-        // use that for 'interval' below.
-        // 'quarterday' is a unique name for our custom period
-        $array['quarterday'] = [
-            'interval' => $period,
-            'display' => 'Every 24 hours',
-        ];
+	public function addCsCron() {
+		error_log( __METHOD__ . ' +' . __LINE__ . PHP_EOL );
 
-        return $array;
-    }
+		// set our 24 hours, units in seconds
+		$period = 24 * 3600;
+		// use that for 'interval' below.
+		// 'quarterday' is a unique name for our custom period
+		$array['quarterday'] = array(
+			'interval' => $period,
+			'display'  => 'Every 24 hours',
+		);
 
-    public static function runOnActivate()
-    {
-        error_log(__METHOD__ . ' +' . __LINE__ . PHP_EOL);
+		return $array;
+	}
 
-        if (! wp_next_scheduled('updateSchedulerNotify')) {
-            wp_schedule_event( time(), 'daily', 'updateSchedulerNotify' );
-        }
-    }
+	public static function runOnActivate() {
+		error_log( __METHOD__ . ' +' . __LINE__ . PHP_EOL );
 
-    public static function runOnDeactivate()
-    {
-        wp_clear_scheduled_hook('updateSchedulerNotify');
-    }
+		if ( ! wp_next_scheduled( 'updateSchedulerNotify' ) ) {
+			wp_schedule_event( time(), 'daily', 'updateSchedulerNotify' );
+		}
+	}
 
-    public function readSpreadSheetData()
-    {
-        error_log(__METHOD__ . ' +' . __LINE__ . PHP_EOL);
+	public static function runOnDeactivate() {
+		wp_clear_scheduled_hook( 'updateSchedulerNotify' );
+	}
 
-        $pluginOptions = get_option(self::PROGRESS_METER_OPTIONS, false);
-        if (($pluginOptions !== false) && is_array($pluginOptions) && !empty($pluginOptions)) {
-            error_log(__METHOD__ . ' +' . __LINE__ . ' Got saved $pluginOptions: ' . var_export($pluginOptions, true) . PHP_EOL);
-            // TDOD: if we already have saved data do not make a request?
-            echo '<script type="text/javascript">' . PHP_EOL .
-                ' let config = {};' . PHP_EOL .
-                ' console.log("+' . __LINE__ . ' config: ", config);' . PHP_EOL .
-                ' config.campaignName = "' . $pluginOptions[0] . '";' . PHP_EOL .
-                ' config.targetAmount = "' . $pluginOptions[1] . '";' . PHP_EOL .
-                ' config.currentAmount = "' . $pluginOptions[2] . '";' . PHP_EOL .
-                ' config.startingAmount = "' . $pluginOptions[3] . '";' . PHP_EOL .
-                ' config.currency = "' . $pluginOptions[4] . '";' . PHP_EOL .
-                ' console.log("+' . __LINE__ . ' config: ", config);' . PHP_EOL .
-                '</script>';
+	public function readSpreadSheetData() {
+		error_log( __METHOD__ . ' +' . __LINE__ . PHP_EOL );
 
-            return true;
-        }
+		$pluginOptions = get_option( self::PROGRESS_METER_OPTIONS, false );
+		if ( ( $pluginOptions !== false ) && is_array( $pluginOptions ) && ! empty( $pluginOptions ) ) {
+			error_log( __METHOD__ . ' +' . __LINE__ . ' Got saved $pluginOptions: ' . var_export( $pluginOptions, true ) . PHP_EOL );
+			// TDOD: if we already have saved data do not make a request?
+			echo '<script type="text/javascript">' . PHP_EOL .
+				' let config = {};' . PHP_EOL .
+				' console.log("+' . __LINE__ . ' config: ", config);' . PHP_EOL .
+				' config.campaignName = "' . $pluginOptions[0] . '";' . PHP_EOL .
+				' config.targetAmount = "' . $pluginOptions[1] . '";' . PHP_EOL .
+				' config.currentAmount = "' . $pluginOptions[2] . '";' . PHP_EOL .
+				' config.startingAmount = "' . $pluginOptions[3] . '";' . PHP_EOL .
+				' config.currency = "' . $pluginOptions[4] . '";' . PHP_EOL .
+				' console.log("+' . __LINE__ . ' config: ", config);' . PHP_EOL .
+				'</script>';
 
-        // Reading data from spreadsheet.
-        $client = new \Google_Client();
-        $client->setApplicationName('ALL Dev');
-        $client->setScopes([\Google_Service_Sheets::SPREADSHEETS]);
-        $client->setAccessType('offline');
-        $client->setAuthConfig(__DIR__ . '/credentials.json');
-        $service = new Google_Service_Sheets($client);
-        $spreadsheetId = '1qyjOXkxijm5cPXSwhKt6RKDwvEfDKU1OMwqNxDiUhPw'; // It is present in your URL
-        // https://docs.google.com/spreadsheets/d/1qyjOXkxijm5cPXSwhKt6RKDwvEfDKU1OMwqNxDiUhPw/edit#gid=0
-        $getRange = 'A2:E2';
-        // Request to get data from spreadsheet.
-        $response = $service->spreadsheets_values->get($spreadsheetId, $getRange);
-        $values = $response->getValues();
-        // error_log(__METHOD__ . ' +' . __LINE__ . ' $values: ' . var_export($values, true) . PHP_EOL);
+			return true;
+		}
 
-        if (isset($values[0]) && is_array($values[0]) && !empty($values[0])) {
-            if ($pluginOptions === false) {
-                add_option(self::PROGRESS_METER_OPTIONS, $values[0]);
-                // error_log(__METHOD__ . ' +' . __LINE__ . ' Added ' . self::PROGRESS_METER_OPTIONS . ' $values[0]: ' . var_export($values[0], true) . PHP_EOL);
-            } else {
-                update_option(self::PROGRESS_METER_OPTIONS, $values[0]);
-                // error_log(__METHOD__ . ' +' . __LINE__ . ' Updated ' . self::PROGRESS_METER_OPTIONS . ' $values[0]: ' . var_export($values[0], true) . PHP_EOL);
-            }
+		// Reading data from spreadsheet.
+		$client = new \Google_Client();
+		$client->setApplicationName( 'ALL Dev' );
+		$client->setScopes( array( \Google_Service_Sheets::SPREADSHEETS ) );
+		$client->setAccessType( 'offline' );
+		$client->setAuthConfig( __DIR__ . '/credentials.json' );
+		$service       = new Google_Service_Sheets( $client );
+		$spreadsheetId = '1qyjOXkxijm5cPXSwhKt6RKDwvEfDKU1OMwqNxDiUhPw'; // It is present in your URL
+		// https://docs.google.com/spreadsheets/d/1qyjOXkxijm5cPXSwhKt6RKDwvEfDKU1OMwqNxDiUhPw/edit#gid=0
+		$getRange = 'A2:E2';
+		// Request to get data from spreadsheet.
+		$response = $service->spreadsheets_values->get( $spreadsheetId, $getRange );
+		$values   = $response->getValues();
+		// error_log(__METHOD__ . ' +' . __LINE__ . ' $values: ' . var_export($values, true) . PHP_EOL);
 
-            echo '<script type="text/javascript">' . PHP_EOL .
-                ' let config = {};' . PHP_EOL .
-                ' console.log("+' . __LINE__ . ' config: ", config);' . PHP_EOL .
-                ' config.campaignName = "' . $values[0][0] . '";' . PHP_EOL .
-                ' config.targetAmount = "' . $values[0][1] . '";' . PHP_EOL .
-                ' config.currentAmount = "' . $values[0][2] . '";' . PHP_EOL .
-                ' config.startingAmount = "' . $values[0][3] . '";' . PHP_EOL .
-                ' config.currency = "' . $values[0][4] . '";' . PHP_EOL .
-                ' console.log("+' . __LINE__ . ' config: ", config);' . PHP_EOL .
-                '</script>';
+		if ( isset( $values[0] ) && is_array( $values[0] ) && ! empty( $values[0] ) ) {
+			if ( $pluginOptions === false ) {
+				add_option( self::PROGRESS_METER_OPTIONS, $values[0] );
+				// error_log(__METHOD__ . ' +' . __LINE__ . ' Added ' . self::PROGRESS_METER_OPTIONS . ' $values[0]: ' . var_export($values[0], true) . PHP_EOL);
+			} else {
+				update_option( self::PROGRESS_METER_OPTIONS, $values[0] );
+				// error_log(__METHOD__ . ' +' . __LINE__ . ' Updated ' . self::PROGRESS_METER_OPTIONS . ' $values[0]: ' . var_export($values[0], true) . PHP_EOL);
+			}
 
-            return true;
-        }
-    }
+			echo '<script type="text/javascript">' . PHP_EOL .
+				' let config = {};' . PHP_EOL .
+				' console.log("+' . __LINE__ . ' config: ", config);' . PHP_EOL .
+				' config.campaignName = "' . $values[0][0] . '";' . PHP_EOL .
+				' config.targetAmount = "' . $values[0][1] . '";' . PHP_EOL .
+				' config.currentAmount = "' . $values[0][2] . '";' . PHP_EOL .
+				' config.startingAmount = "' . $values[0][3] . '";' . PHP_EOL .
+				' config.currency = "' . $values[0][4] . '";' . PHP_EOL .
+				' console.log("+' . __LINE__ . ' config: ", config);' . PHP_EOL .
+				'</script>';
 
-    private function deleteOptions()
-    {
-        delete_option(self::PROGRESS_METER_OPTIONS);
+			return true;
+		}
+	}
 
-        return true;
-    }
+	private function deleteOptions() {
+		delete_option( self::PROGRESS_METER_OPTIONS );
 
-    public function progressMeterWidget()
-    {
-        error_log(__METHOD__ . ' +' . __LINE__ . PHP_EOL);
+		return true;
+	}
 
-        wp_enqueue_script("progressMeterScript",  plugin_dir_url( __FILE__ ) . "assets/js/progress-meter.js", [], self::$VER, true);
-        wp_enqueue_style("progressMeterStyle",  plugin_dir_url( __FILE__ ) . "assets/css/progress-meter.css", [], self::$VER);
+	public function progressMeterWidget() {
+		error_log( __METHOD__ . ' +' . __LINE__ . PHP_EOL );
 
-        // Read Google Spreadsheet data
-        $this->readSpreadSheetData();
+		wp_enqueue_script( 'progressMeterScript', plugin_dir_url( __FILE__ ) . 'assets/js/progress-meter.js', array(), self::$VER, true );
+		wp_enqueue_style( 'progressMeterStyle', plugin_dir_url( __FILE__ ) . 'assets/css/progress-meter.css', array(), self::$VER );
 
-        $html = <<<EOH
+		// Read Google Spreadsheet data
+		$this->readSpreadSheetData();
+
+		$html = <<<EOH
 <div class="progress-meter-wrapper">
 	<div class="progress-meter-thermo-heading"></div>
 	<div class="progress-meter-termometer">
@@ -211,55 +201,53 @@ class Progress_Meter
 	</div>
 </div>
 EOH;
-        echo $html;
-    }
+		echo $html;
+	}
 
-    public function removePluginData()
-    {
-        error_log(__METHOD__ . ' +' . __LINE__ . PHP_EOL);
+	public function removePluginData() {
+		error_log( __METHOD__ . ' +' . __LINE__ . PHP_EOL );
 
-        return $this->deleteOptions();
-    }
+		return $this->deleteOptions();
+	}
 
-    public function updatePluginData()
-    {
-        error_log(__METHOD__ . ' +' . __LINE__ . PHP_EOL);
+	public function updatePluginData() {
+		error_log( __METHOD__ . ' +' . __LINE__ . PHP_EOL );
 
-        $pluginOptions = get_option(self::PROGRESS_METER_OPTIONS, false);
-        if (($pluginOptions !== false) && is_array($pluginOptions) && !empty($pluginOptions)) {
-            error_log(__METHOD__ . ' +' . __LINE__ . ' Got saved $pluginOptions: ' . var_export($pluginOptions, true) . PHP_EOL);
+		$pluginOptions = get_option( self::PROGRESS_METER_OPTIONS, false );
+		if ( ( $pluginOptions !== false ) && is_array( $pluginOptions ) && ! empty( $pluginOptions ) ) {
+			error_log( __METHOD__ . ' +' . __LINE__ . ' Got saved $pluginOptions: ' . var_export( $pluginOptions, true ) . PHP_EOL );
 
-            return true;
-        }
+			return true;
+		}
 
-        // Reading data from spreadsheet.
-        $client = new \Google_Client();
-        $client->setApplicationName('ALL Dev');
-        $client->setScopes([\Google_Service_Sheets::SPREADSHEETS]);
-        $client->setAccessType('offline');
-        $client->setAuthConfig(__DIR__ . '/credentials.json');
-        $service = new Google_Service_Sheets($client);
-        $spreadsheetId = '1qyjOXkxijm5cPXSwhKt6RKDwvEfDKU1OMwqNxDiUhPw';
-        $getRange = 'A2:E2';
-        $response = $service->spreadsheets_values->get($spreadsheetId, $getRange);
-        $values = $response->getValues();
+		// Reading data from spreadsheet.
+		$client = new \Google_Client();
+		$client->setApplicationName( 'ALL Dev' );
+		$client->setScopes( array( \Google_Service_Sheets::SPREADSHEETS ) );
+		$client->setAccessType( 'offline' );
+		$client->setAuthConfig( __DIR__ . '/credentials.json' );
+		$service       = new Google_Service_Sheets( $client );
+		$spreadsheetId = '1qyjOXkxijm5cPXSwhKt6RKDwvEfDKU1OMwqNxDiUhPw';
+		$getRange      = 'A2:E2';
+		$response      = $service->spreadsheets_values->get( $spreadsheetId, $getRange );
+		$values        = $response->getValues();
 
-        if (isset($values[0]) && is_array($values[0]) && !empty($values[0])) {
-            if ($pluginOptions === false) {
-                add_option(self::PROGRESS_METER_OPTIONS, $values[0]);
+		if ( isset( $values[0] ) && is_array( $values[0] ) && ! empty( $values[0] ) ) {
+			if ( $pluginOptions === false ) {
+				add_option( self::PROGRESS_METER_OPTIONS, $values[0] );
 
-                error_log(__METHOD__ . ' +' . __LINE__ . ' Added ' . self::PROGRESS_METER_OPTIONS . ' $values[0]: ' . var_export($values[0], true) . PHP_EOL);
-            } else {
-                update_option(self::PROGRESS_METER_OPTIONS, $values[0]);
+				error_log( __METHOD__ . ' +' . __LINE__ . ' Added ' . self::PROGRESS_METER_OPTIONS . ' $values[0]: ' . var_export( $values[0], true ) . PHP_EOL );
+			} else {
+				update_option( self::PROGRESS_METER_OPTIONS, $values[0] );
 
-                error_log(__METHOD__ . ' +' . __LINE__ . ' Updated ' . self::PROGRESS_METER_OPTIONS . ' $values[0]: ' . var_export($values[0], true) . PHP_EOL);
-            }
+				error_log( __METHOD__ . ' +' . __LINE__ . ' Updated ' . self::PROGRESS_METER_OPTIONS . ' $values[0]: ' . var_export( $values[0], true ) . PHP_EOL );
+			}
 
-            return true;
-        }
+			return true;
+		}
 
-        return false;
-    }
+		return false;
+	}
 }
 
 /**
@@ -271,19 +259,16 @@ EOH;
  *
  * @return Progress_Meter
  * @since      1.0.0
- *
  */
-function pm()
-{
-    return Progress_Meter::instance();
+function pm() {
+	 return Progress_Meter::instance();
 }
 
-function pm_init()
-{
-    $progressMeter = pm();
+function pm_init() {
+	$progressMeter = pm();
 
-    register_activation_hook(__FILE__, [$progressMeter, 'runOnActivate']);
-    register_deactivation_hook(__FILE__, [$progressMeter, 'runOnDeactivate']);
+	register_activation_hook( __FILE__, array( $progressMeter, 'runOnActivate' ) );
+	register_deactivation_hook( __FILE__, array( $progressMeter, 'runOnDeactivate' ) );
 }
 
 // Get Progress Meter running
